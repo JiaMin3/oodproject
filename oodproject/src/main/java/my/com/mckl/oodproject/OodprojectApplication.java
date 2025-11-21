bf901f73935b71120e144f4df3e9657c0551c586
Jia Min <ng.jm.30587@mckl2.edu.my>
2025-11-21 21:36:38 +0800

    asdfghhjk

---
+++ oodproject/src/main/java/my/com/mckl/oodproject/OodprojectApplication.java
@@ -0,0 +1,13 @@
+package my.com.mckl.oodproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OodprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OodprojectApplication.class, args);
+	}
+
+}
---
+++ oodproject/src/test/java/my/com/mckl/oodproject/OodprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package my.com.mckl.oodproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OodprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
